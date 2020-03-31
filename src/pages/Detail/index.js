f3bb257f1ff038d1552edc5a65133ab0645e3443
Nilo Neregato <neregato.nilo@gmail.com>
2020-03-31 23:50:22 +0200

    send whatsapp msg

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StatusBar} from 'react-native';
+import {Image, StatusBar, Linking} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import email from 'react-native-email';
@@ -24,7 +24,7 @@ import {
 export default function Detail() {
   const navigation = useNavigation();
   const message =
-    'Hello, I am contacting as I am interesting to help the case "Help My dog" byt donation $120,00';
+    'Hello, I am contacting as I am interesting to help the case "Help My dog" by donating $120,00';
 
   function navigateBack() {
     navigation.goBack();
@@ -37,7 +37,9 @@ export default function Detail() {
     }).catch(console.error);
   }
 
-  function sendWhatsApp() {}
+  function sendWhatsApp() {
+    Linking.openURL(`whatsapp://send?phone=123456789&text=${message}`);
+  }
 
   return (
     <Container statusBarHeight={StatusBar.currentHeight}>
@@ -63,7 +65,7 @@ export default function Detail() {
         <HeroTitle>Be the hero of this case.</HeroTitle>
         <HeroDescription>Contact:</HeroDescription>
         <Actions>
-          <ActionButton onPress={() => {}}>
+          <ActionButton onPress={sendWhatsApp}>
             <ButtonDescription>WhatsApp</ButtonDescription>
           </ActionButton>
           <ActionButton onPress={sendMail}>
