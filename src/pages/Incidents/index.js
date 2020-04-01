dc66e9c543909af4cb52746241b403aba8eb2644
Nilo Neregato <neregato.nilo@gmail.com>
2020-04-01 22:27:16 +0200

    fetch data from be with pagination

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Image, StatusBar} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
+
+import api from '../../service/api';
 
 import {
   Container,
@@ -22,38 +24,80 @@ import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
   const navigation = useNavigation();
+  const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-  function navigateToDetail() {
-    navigation.navigate('Detail');
+  function navigateToDetail(incident) {
+    navigation.navigate('Detail', {incident});
   }
+
+  async function loadIncidents() {
+    if (loading || (total > 0 && incidents.length === total)) {
+      return;
+    }
+
+    setLoading(true);
+
+    await api
+      .get('incidents', {
+        params: {
+          page,
+        },
+      })
+      .then((response) => {
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+      })
+      .finally(() => {
+        setPage(page + 1);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    loadIncidents();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Container statusBarHeight={StatusBar.currentHeight}>
       <Header>
         <Image source={logoImg} />
         <HeaderText>
-          Total of <TotalText>0 cases</TotalText>
+          Total of <TotalText>{total} cases</TotalText>
         </HeaderText>
       </Header>
       <Greetings>Welcome!</Greetings>
       <Description>Choose a case below and save the day.</Description>
 
       <IncidentList
-        data={[1, 2, 3]}
-        keyExtractor={(incident) => String(incident)}
-        showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        data={incidents}
+        keyExtractor={(incident) => String(incident.id)}
+        showsVerticalScrollIndicator={true}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
+        renderItem={({item: incident}) => (
           <Incident>
             <IncitendProperty>ONG:</IncitendProperty>
-            <IncidentValue>APAD</IncidentValue>
+            <IncidentValue>{incident.name}</IncidentValue>
 
             <IncitendProperty>CASO:</IncitendProperty>
-            <IncidentValue>Help the dog</IncidentValue>
+            <IncidentValue>{incident.title}</IncidentValue>
 
             <IncitendProperty>VALUE:</IncitendProperty>
-            <IncidentValue>$120,00</IncidentValue>
+            <IncidentValue>
+              {Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              }).format(incident.value)}
+            </IncidentValue>
 
-            <Buton onPress={navigateToDetail}>
+            <Buton
+              onPress={() => {
+                navigateToDetail(incident);
+              }}>
               <ButtonText>Mode details</ButtonText>
               <Icon name="arrow-right" size={16} color="#e02041" />
             </Buton>
