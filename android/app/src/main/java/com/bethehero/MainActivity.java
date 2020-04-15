5e8109da41650ad2bc2c56a40f225c1952e21903
Nilo Neregato <neregato.nilo@gmail.com>
2020-04-16 00:21:54 +0200

    fix build app

--- android/app/src/main/java/com/bethehero/MainActivity.java
+++ android/app/src/main/java/com/bethehero/MainActivity.java
@@ -1,4 +1,4 @@
-package com.betheheromobile;
+package com.bethehero;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "betheheromobile";
+    return "bethehero";
   }
 }
--- ios/betheheroTests/betheheroTests.m
+++ ios/betheheroTests/betheheroTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface betheheromobileTests : XCTestCase
+@interface betheheroTests : XCTestCase
 
 @end
 
-@implementation betheheromobileTests
+@implementation betheheroTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
